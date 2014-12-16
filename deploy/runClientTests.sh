33d085a32ac1b50a225794cf618ed8e888c88607
reimerei <github@bjrm.de>
2014-12-16 18:32:23 +0100

    remember exit status

--- deploy/runClientTests.sh
+++ deploy/runClientTests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -e
-
 imageName="cameo-test"
 imagePort=9000
 embedMongoFile="/opt/mongodb-linux-x86_64-2.6.5.tgz"
@@ -24,7 +22,7 @@ containerId=$(sudo docker ps | grep ${imageName} | cut -f1 -d' ')
 timeout=50
 while [ -z "${log}" ] && [ "$timeout" -gt 0 ]; do
 	echo waiting for container to start. Patience left: ${timeout}
-	log=$(sudo docker logs ${containerId} | grep Listening || true)
+	log=$(sudo docker logs ${containerId} | grep Listening)
 	sleep 2	
 	timeout=`expr $timeout - 1` 
 done
@@ -32,9 +30,11 @@ done
 cd cameoJSClient
 ./test.sh test http://localhost:${imagePort}/m/ http://localhost:${imagePort}/a/v1 
 
+exitStatus=$?
+
 echo -e "\e[33m[ CameoTest - Stopping test container ]\033[0m"
 sudo docker stop ${containerId}
 echo -e "\e[33m[ CameoTest - Removing test container ]\033[0m"
 sudo docker rm ${containerId}
 
-
+exit $exitStatus
