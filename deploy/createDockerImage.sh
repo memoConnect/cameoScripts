7a80143e237d4f67015be0f96e4254fc441be89b
reimerei <github@bjrm.de>
2015-01-21 17:47:02 +0100

    fix

--- deploy/createDockerImage.sh
+++ deploy/createDockerImage.sh
@@ -13,8 +13,5 @@ cd docker
 sudo docker build -t ${1} --rm .
 
 if [ "$2" == "push" ]; then
-	domain=$(echo ${1} | cut -d'/' -f1) 
-	echo "Pushing to: "${1}
-	echo "Domain: "${domain}
-	sudo docker --insecure-registry ${domain} push ${1}
+	sudo docker push ${1}
 fi	
